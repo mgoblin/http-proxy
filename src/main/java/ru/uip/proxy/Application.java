f2b61c3c7d29e0a482aba45f60006e362f56c74d
mgoblin <mike.golovanov@gmail.com>
2020-07-22 22:09:41 +0300

    java DSL route with modify body

--- src/main/java/ru/uip/proxy/Application.java
+++ src/main/java/ru/uip/proxy/Application.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 public class Application {
@@ -13,7 +15,24 @@ public class Application {
     }
 
     @Bean
-    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
-        return builder.routes().build();
+    public RouteLocator routes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("upper_route", r -> r
+                        .method(HttpMethod.GET, HttpMethod.POST)
+                        .and()
+                        .path("/upper")
+                        .filters(f -> f
+                                .rewritePath(".*", "/")
+                                .modifyResponseBody(
+                                        String.class,
+                                        String.class,
+                                        (exchange, s) -> {
+                                            return Mono.just(s.toUpperCase());
+                                        }
+                                )
+                        )
+                        .uri("https://yandex.ru")
+                )
+                .build();
     }
 }
